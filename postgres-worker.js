0ac9686cd7ff768896ac68712caa892dac53b619
artificial-life <proteus.protocol@gmail.com>
2017-01-20 03:27:55 +0700

    pg worker

--- postgres-worker.js
+++ postgres-worker.js
@@ -1,18 +1,23 @@
 "use strict"
 
-const connect = require('./post-connect.js');
+const pg = require('./post-connect.js');
 const _ = require('lodash');
 
 class PGWorker {
-  constructor() {
+	constructor() {
 
-  }
-  getGroup(data) {
-    return {};
-    // _.map(v, 'timestamp').join(',')
-  }
+	}
+	getGroup(grp) {
+		let query_string = `SELECT posts.id, posts.text, posts.timestamp, users.id, users.name, users.pic FROM posts  INNER JOIN users ON posts.user_id = users.id WHERE posts.group_id = ${grp} ORDER BY posts.timestamp DESC LIMIT 10`;
+		return new Promise((resolve, reject) => {
+				this._pg.query(query_string, (err, result) => {
+					if (err) reject(new Error(err));
+					resolve(result);
+				});
+			})
+			.then(res => res.rows);
+	}
 }
 
-app.get('method', () => {})
 
 module.exports = PGWorker;
